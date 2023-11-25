89110dd7add7dd32971c5d5ad725b4257f47bbb2
Takashi Imahiro <imahiro@erin.jp>
2023-11-25 15:42:14 +0900

    change version to 1.6.0

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The package can be installed by adding `auth0_api` to your list of dependencies 
 ```elixir
 def deps do
   [
-    {:auth0_api, "~> 1.5.2"}
+    {:auth0_api, "~> 1.6.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Auth0Api.MixProject do
   def project do
     [
       app: :auth0_api,
-      version: "1.5.2",
+      version: "1.6.0",
       elixir: "~> 1.8",
       name: "Auth0Api",
       description: @description,
